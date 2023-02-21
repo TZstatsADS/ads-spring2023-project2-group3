6b880e97f05d5f13a9f97d8ca673d0922076a0da
kaityb24 <69737821+kaityb24@users.noreply.github.com>
2023-02-21 18:30:16 -0500

    Delete GenderAPICall.R

--- lib/GenderAPICall.R
+++
@@ -1,7 +0,0 @@
-#use genderizeR package to create dataset with gender of all first names in payroll data
-df = read.csv('ads-spring2023-project2-group3-main/data/Citywide_Payroll_Data__Fiscal_Year_.csv')
-x = c(unique(df$First.Name))
-key = 'private'
-givenNames = findGivenNames(x, apikey = key)
-write.csv(givenNames, "ads-spring2023-project2-group3-main/data/NameGender.csv")
-
